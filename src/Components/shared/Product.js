cc019e2bd6c7b66b6b2d5bbcc62e4be84455ddfd
Amirhossein zanganeh <amirzanganeh1999@gmail.com>
2021-10-12 22:03:28 +0330

    .

--- src/Components/shared/Product.js
+++ src/Components/shared/Product.js
@@ -19,15 +19,15 @@ const Product=({productsData}) =>{
             <h3>{productsData.price} $</h3>
         <div>
             <Link to={`/product/${productsData.id}`}>Details</Link>
-            <div>{
-                
-                isInCart(state , productsData.id) ?
-                <button onClick={() => dispatch({type: "INCREASE", payload: productsData})}>+</button>:
-                <button onClick={() => dispatch({type: "ADD_ITEM", payload: productsData})}>Add to Cart</button>
+            <div>
+            {quantityCount(state, productsData.id ) > 1 && <button onClick={() => dispatch({type:"DECREASE", payload:productsData})}>-</button>}
+            {quantityCount(state, productsData.id ) === 1 && <button onClick={() => dispatch({type:"REMOVE_ITEM", payload:productsData})}>remove</button>}
+                {
+                    isInCart(state , productsData.id) ?
+                    <button onClick={() => dispatch({type: "INCREASE", payload: productsData})}>+</button>:
+                    <button onClick={() => dispatch({type: "ADD_ITEM", payload: productsData})}>Add to Cart</button>
 
                 }
-                {quantityCount(state, productsData.id === 1) && <button onClick={() => dispatch({type:"DECREASE", payload:productsData})}>-</button>}
-                {quantityCount(state, productsData.id > 1) && <button onClick={() => dispatch({type:"REMOVE_ITEM", payload:productsData})}>remove</button>}
 
 
             </div>
