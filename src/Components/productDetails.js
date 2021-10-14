ddb1306f63d0da4ce03888b7cbf4213cbcccb024
Amirhossein zanganeh <amirzanganeh1999@gmail.com>
2021-10-14 17:34:31 +0330

    end

--- src/Components/productDetails.js
+++ src/Components/productDetails.js
@@ -1,20 +1,25 @@
 import React, { useContext } from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import styles from "../assets/styles/details.module.css";
 import { ProductContext } from "../context/ProductContextProvider";
 const ProductDetails = (props) => {
-    const id = props.match.params.id;
-    const data = useContext(ProductContext);
-    const product= data[id - 1];
-    const {image, title, description, price, category} = product;
+  const id = props.match.params.id;
+  const data = useContext(ProductContext);
+  const product = data[id - 1];
+  const { image, title, description, price, category } = product;
 
   return (
-    <div>
-     <img src={image} alt="product"/>
-     <h1>{title}</h1>
-     <p>{description}</p>
-     <p>{category}</p>
-     <h3>{price} $</h3>
-     <Link to="/product">Back to start </Link>
+    <div className={styles.main}>
+        <img src={image} alt="product" />
+      <div className={styles.details}>
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <p><span>Category's:</span> {category}</p>
+        <h3>{price} $</h3>
+        <Link to="/product" className={styles.link}>Back to start </Link>
+      </div>
+
+      
     </div>
   );
 };
--- src/Functions/functions.js
+++ src/Functions/functions.js
@@ -1,6 +1,6 @@
 const shorter = (title) => {
   const splitedtitle = title.split(" ");
-  const newtitle = `${splitedtitle[0]} ${splitedtitle[1]}`;
+  const newtitle = `${splitedtitle[0]}`;
   return newtitle;
 };
 
